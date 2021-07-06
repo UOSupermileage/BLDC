fe468461577b9d7318db7027f379ddae6bb9373a
4DoorToyota <cmabo071@uottawa.ca>
2021-07-05 20:12:06 -0400

    Separated the core clock increasing functions as well as the pin declerations out into their own files to increase the usability of the code

---
+++ CSS-Project/coreFunctions.h
@@ -0,0 +1,21 @@
+/*
+ * coreFunctions.h
+ *
+ * Contains all of the functions responsible for chaning the core speed of the processor
+ *
+ *  Created on: Jul 5, 2021
+ *      Author: Camry
+ */
+
+#ifndef COREFUNCTIONS_H_
+#define COREFUNCTIONS_H_
+
+namespace coreFunctions{
+
+void SetVcoreUp (unsigned int level);
+
+void IncreaseClockSpeed_25MHz();
+
+}
+
+#endif /* COREFUNCTIONS_H_ */
---
+++ CSS-Project/pinDeclerations.h
@@ -0,0 +1,41 @@
+/*
+ * UOE Racing: Pin dec header for motor controller program
+ * V1.0
+ *
+ *
+ * Written by Camry Mabon
+ *
+ * DO NOT CHANGE unless you have a good understanding of what your changes will do and you have checked with a member of the electrical team
+ */
+
+namespace PINS{
+
+#define ENABLE 5
+
+// SPI PINS
+#define SCLK 7
+#define nFAULT 33
+
+#define CS_TEMP 32
+#define CS_SD 11
+#define EXTERNAL_SPI_ENABLE 15
+
+#define CS_DRV 8
+
+// HALL PINS
+#define HALLA 19
+#define HALLB 13
+#define HALLC 12
+
+#define LOWA 39
+#define LOWB 37
+#define LOWC 35
+
+#define HIGHA 40
+#define HIGHB 38
+#define HIGHC 36
+
+#define THROTTLE 24
+
+
+}
